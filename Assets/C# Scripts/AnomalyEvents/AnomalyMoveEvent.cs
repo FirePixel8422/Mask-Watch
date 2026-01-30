caa83e9129cd76a4459e10083e2b6990050d76e3
FirePixel8422 <bmtminecraft@hotmail.com>
2026-01-30 11:02:36 +0100

    gizmo scale fix for move and tp event

--- Assets/C# Scripts/AnomalyEvents/AnomalyMoveEvent.cs
+++ Assets/C# Scripts/AnomalyEvents/AnomalyMoveEvent.cs
@@ -45,7 +45,10 @@ public class AnomalyMoveEvent : AnomalyEvent
     private void OnDrawGizmosSelected()
     {
         Transformation parentTransformation = transform.parent != null ? new Transformation(transform.parent) : Transformation.Identity;
-        Gizmos.DrawWireMesh(GetComponent<MeshFilter>().sharedMesh, 0, parentTransformation.Position + transformation.Position, parentTransformation.Rotation * transformation.Rotation, parentTransformation.Scale + transformation.Scale);
+        Gizmos.DrawWireMesh(GetComponent<MeshFilter>().sharedMesh, 0,
+            parentTransformation.Position + transformation.Position,
+            parentTransformation.Rotation * transformation.Rotation,
+            Vector3.Cross(parentTransformation.Scale, transformation.Scale));
     }
 
 #if UNITY_EDITOR
--- Assets/C# Scripts/AnomalyEvents/AnomalyTeleportEvent.cs
+++ Assets/C# Scripts/AnomalyEvents/AnomalyTeleportEvent.cs
@@ -14,7 +14,10 @@ public class AnomalyTeleportEvent : AnomalyEvent
     private void OnDrawGizmosSelected()
     {
         Transformation parentTransformation = transform.parent != null ? new Transformation(transform.parent) : Transformation.Identity;
-        Gizmos.DrawWireMesh(GetComponent<MeshFilter>().sharedMesh, 0, parentTransformation.Position + transformation.Position, parentTransformation.Rotation * transformation.Rotation, parentTransformation.Scale + transformation.Scale);
+        Gizmos.DrawWireMesh(GetComponent<MeshFilter>().sharedMesh, 0,
+            parentTransformation.Position + transformation.Position,
+            parentTransformation.Rotation * transformation.Rotation,
+            Vector3.Cross(parentTransformation.Scale, transformation.Scale));
     }
 
 
